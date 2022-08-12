994ca2c884eb82e46f719217216cd64d577ff565
Moosems <joshua_yacktman@windermereprep.com>
2022-08-12 07:46:25 -0400

    Take 11

--- DIP.py
+++ DIP.py
@@ -3,7 +3,7 @@ from tkinter import Tk, Label
 class ExampleApp(Tk):
     def __init__(self):
         super().__init__()
-        self.label = Label(self, text="Hello World")
+        self.label = Label(self, text="Hello World", font=("Courier New bold", 15))
         self.label.pack()
 
 def create_application():
